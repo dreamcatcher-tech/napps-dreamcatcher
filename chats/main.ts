7535190e4f2aad053d7497a7763f2afe4429e2a7
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-28 22:37:27 +0000

    ghactions

--- chats/main.ts
+++ chats/main.ts
@@ -1,8 +1,20 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import type { Implementations } from '@artifact/client/tools'
+import type schema from './schema.ts'
+import { pushable } from 'it-pushable'
+type Tools = Implementations<typeof schema>
+
+export const newChat: Tools['newChat'] = async ({ config }) => {
+  console.log(config)
+  return {
+    chatId: '123',
+  }
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export const infer: Tools['infer'] = () => {
+  const stream = pushable<string>({ objectMode: true })
+  stream.push('123')
+  stream.push('456')
+  stream.push('789')
+  stream.end()
+  return stream
 }
---
+++ chats/schema.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod'
+
+export default {
+  name: '@dreamcatcher/chats',
+  tools: {
+    newChat: {
+      parameters: z.object({ config: z.string() }),
+      returns: z.object({
+        chatId: z.string(),
+      }),
+    },
+
+    infer: {
+      parameters: z.object({}),
+      stream: z.string(),
+    },
+  },
+} as NappShape
+
+type NappShape = {
+  name: '@dreamcatcher/chats'
+  tools: {
+    newChat: {
+      parameters: z.ZodObject<{
+        config: z.ZodString
+      }>
+      returns: z.ZodObject<{
+        chatId: z.ZodString
+      }>
+    }
+    infer: {
+      parameters: z.ZodObject<{}>
+      stream: z.ZodString
+    }
+  }
+}
