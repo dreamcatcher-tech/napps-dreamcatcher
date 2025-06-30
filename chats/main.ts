33521568d113975e5b6668c502eb008806037c50
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-30 04:54:33 +0000

    upgrades

--- chats/main.ts
+++ chats/main.ts
@@ -1,19 +1,37 @@
+import { ulid } from 'ulid'
 import type { Implementations } from '@artifact/client/tools'
+import { useArtifact } from '@artifact/utils-server'
 import type schema from './schema.ts'
-import { streamText } from 'ai'
+import {
+  assistantModelMessageSchema,
+  DefaultChatTransport,
+  streamText,
+  systemModelMessageSchema,
+  toolModelMessageSchema,
+  userModelMessageSchema,
+} from 'ai'
+// message will always be one of these types, as well as:
+// 1. a config message, which represents model settings for the call
+// 2. transcludes, which represent bundles of context
+
 import { openai } from '@ai-sdk/openai'
 type Tools = Implementations<typeof schema>
 
 export const newChat: Tools['newChat'] = async ({ config }) => {
   // config can be used here for initialization
-  return {
-    chatId: '123',
-  }
+  const chatId = ulid()
+  const filename = `chats/${chatId}/config.json`
+  const artifact = useArtifact()
+
+  artifact.files.write.json(filename, config)
+  await artifact.branch.write.commit('new chat: ' + chatId)
+
+  return { chatId }
 }
 
 export const infer: Tools['infer'] = () => {
   const { textStream, usage, totalUsage } = streamText({
-    model: openai('gpt-4.1'),
+    model: openai.responses('o3-pro'),
     prompt: 'Write a poem about embedding models.',
   })
   return textStream
